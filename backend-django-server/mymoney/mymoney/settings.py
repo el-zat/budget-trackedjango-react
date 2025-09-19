9051be564ff29c043cc7c5c3448dc126c714644f
Elena Zatykina <elena.zatykina@hexagon.com>
2025-09-19 14:32:59 +0200

    adjust static files path

--- backend-django-server/mymoney/mymoney/settings.py
+++ backend-django-server/mymoney/mymoney/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = "mymoney.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        'DIRS': [BASE_DIR /'build'],
+        'DIRS': [BASE_DIR / 'frontend-react' / 'mymoney-react' / 'build'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -139,7 +139,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    BASE_DIR / "frontend-react" / "mymoney-react" / "build" / "static",
+
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
