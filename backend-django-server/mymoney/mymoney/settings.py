b1ad34033b63d58bbb4e38c44d50fe5fc10b8e80
Elena Zatykina <elena.zatykina@hexagon.com>
2025-09-30 12:55:22 +0200

    logging Django in file

--- backend-django-server/mymoney/mymoney/settings.py
+++ backend-django-server/mymoney/mymoney/settings.py
@@ -172,3 +172,23 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'django_debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
